6d84ea90a0bce44d657a90c4e728eb4e8063f2be
Lucas Retamoso <37214243+lucasretamoso@users.noreply.github.com>
2022-04-03 23:07:21 -0300

    ✨ Add the html template

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { router } from './listeners';
 import expressWS from 'express-ws';
+import { routerWeb } from './web';
 
 const appBase = express();
 const wsInstance = expressWS(appBase);
@@ -8,6 +9,7 @@ const { app } = wsInstance;
 
 const routerExpress = express.Router();
 router(routerExpress);
+routerWeb(routerExpress);
 
 app.listen(3000, () => {
   console.log('listening on *:3000');
