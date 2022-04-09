1e680686af48bbbaa613e5ee04855767150def9d
Lucas Retamoso <37214243+lucasretamoso@users.noreply.github.com>
2022-04-08 22:17:15 -0300

    🚚 Move data folder to src

--- src/data/DTOs/bitfinexOrderBookDTO.ts
+++ src/data/DTOs/bitfinexOrderBookDTO.ts
@@ -1,9 +1,9 @@
-import { BitfinexMessageDTO } from "./bitfinexMessageDTO";
+import { BitfinexMessageDTO } from './bitfinexMessageDTO';
 
 export interface BitfinexOrderBookDTO {
   bid: Map<string, string>;
   ask: Map<string, string>;
-};
+}
 
 export interface EffectivePriceDTO extends BitfinexMessageDTO{
   operation: 'buy' | 'sell';
